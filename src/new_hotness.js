92b5f0bd9768683b268062d5412792017f2dd22c
Alex Sears <me@alexsears.com>
2018-12-02 11:05:32 -0500

    Initial commit

---
+++ src/new_hotness.js
@@ -0,0 +1,7 @@
+const newStuff = 'i use semicolons because i like to prevent bugs in my code';
+
+const thisHere = () => {
+  console.log('function is what all the other functions wish they were');
+};
+
+thisHere();
---
+++ src/old_and_busted.js
@@ -0,0 +1,5 @@
+var old_stuff = 'i live on the edge with no semicolons'
+
+const thishere = function(is) {
+        console.log ("all messed up")
+}
